161afe81e32faa731ccd3769541f207da5a58b7a
AbigailSc <abigailsarzuri@gmail.com>
2023-02-09 15:26:18 -0300

    style: navbar and home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,11 +1,11 @@
+import SliderHome from '@components/SliderHome/SliderHome';
 import React from 'react';
-import Profile from '@views/Profile';
 import './Home.scss';
 
 const Home: React.FC = () => {
   return (
     <div className="Home">
-      <Profile />
+      <SliderHome />
     </div>
   );
 };
